80f608f2606e548663d3f4b2f806fc2ae6df34b7
Fabrizio Colonna <colofabrix@tin.it>
2024-08-01 17:42:16 +0100

    Further refinements

---
+++ src/main/g8/src/test/scala/$organization__packaged$/MainSpec.scala
@@ -0,0 +1,13 @@
+package $organization$
+
+import munit.FunSuite
+
+class MainSpec extends FunSuite {
+
+  test("Example test that succeeds") {
+    val obtained = 42
+    val expected = 42
+    assertEquals(obtained, expected)
+  }
+
+}
